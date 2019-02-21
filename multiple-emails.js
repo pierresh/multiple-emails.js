8e31f7fb91e0cf81cd3fa5807c942945aebc5458
John Underwood <John.Underwood@unchealth.unc.edu>
2019-02-21 09:41:23 -0500

    fix bug with making duplicates bold

--- multiple-emails.js
+++ multiple-emails.js
@@ -4,7 +4,7 @@
 
 		// Default options
 		var defaults = {
-			checkDupEmail: true,
+			checkForDuplicates: true,
 			validate: true,
 			theme: "Basic",
 			position: "top",
@@ -28,13 +28,13 @@
 			deleteIconHTML = '<a href="#" class="multiple_emails-close" title="Remove"><span class="textdeleteicon">[Remove]</span></a>';
 		}
 		else { // if (settings.theme.toLowerCase() == "Basic".toLowerCase()) {
-			//Default which you should use if you don't use Bootstrap, SemanticUI, or other CSS frameworks
-			//uses the HTML character 'heavy times sign'; https://www.fileformat.info/info/unicode/char/2716/index.htm
+			// Default which you should use if you don't use Bootstrap, SemanticUI, or other CSS frameworks
+			            // uses the 'heavy multiplication x' HTML entity; https://www.fileformat.info/info/unicode/char/2716/index.htm
 			deleteIconHTML = '<a href="#" class="multiple_emails-close" title="Remove"><span class="basicdeleteicon">&#10006;</span></a>';
 		}
 
 		return this.each(function() {
-			//$orig refers to the input HTML node
+			// $orig refers to the input HTML node
 			var $orig = $(this);
 			var $list = $('<ul class="multiple_emails-ul" />'); // create html elements - list of email addresses as unordered list
 
@@ -60,22 +60,22 @@
 					keynum = e.which;
 				}
 
-				//if(event.which == 8 && input_length == 0) { $list.find('li').last().remove(); } //Removes last item on backspace with no input
+				// if(event.which == 8 && input_length == 0) { $list.find('li').last().remove(); } //Removes last item on backspace with no input
 
 				// Supported key press is tab, enter, space or comma, there is no support for semi-colon since the keyCode differs in various browsers
 				if(keynum == 9 || keynum == 32 || keynum == 188) {
-					display_email($(this), settings.checkDupEmail, settings.validate);
+					display_email($(this), settings.checkForDuplicates, settings.validate);
 				}
 				else if (keynum == 13) {
-					display_email($(this), settings.checkDupEmail, settings.validate);
-					//Prevents enter key default
-					//This is to prevent the form from submitting with  the submit button
-					//when you press enter in the email textbox
+					display_email($(this), settings.checkForDuplicates, settings.validate);
+					// Prevents enter key default
+					// This is to prevent the form from submitting with  the submit button
+					// when you press enter in the email textbox
 					e.preventDefault();
 				}
 
 			}).on('blur', function(event){
-				if ($(this).val() != '') { display_email($(this), settings.checkDupEmail, settings.validate); }
+				if ($(this).val() != '') { display_email($(this), settings.checkForDuplicates, settings.validate); }
 			});
 
 			var $container = $('<div class="multiple_emails-container" />').click(function() { $input.focus(); } ); // container div
@@ -92,34 +92,35 @@
 			As such, the string is tokenized, with each token validated individually if validate
 			variable is set to true.
 
-			If the dupEmailCheck variable is set to true, scans for duplicate emails, and invalidates input if found.
-			Otherwise allows emails to have duplicated values if false.
+			If the checkForDuplicates variable is set to true, scans for duplicate emails, and
+			invalidates input if found. Otherwise allows emails to have duplicated values if false.
 			*/
-			function display_email(t, dupEmailCheck, validate) {
+			function display_email(t, checkForDuplicates, validate) {
 
-				//Remove space, comma and semi-colon from beginning and end of string
-				//Does not remove inside the string as the email will need to be tokenized using space, comma and semi-colon
+				// Remove space, comma and semi-colon from beginning and end of string
+				// Does not remove inside the string as the email will need to be tokenized using space, comma and semi-colon
 				var arr = t.val().trim().replace(/^,|,$/g , '').replace(/^;|;$/g , '');
-				//Remove the double quote
+				// Remove the double quote
 				arr = arr.replace(/"/g,"");
-				//Split the string into an array, with the space, comma, and semi-colon as the separator
+				// Split the string into an array, with the space, comma, and semi-colon as the separator
 				arr = arr.split(/[\s,;]+/);
 
-				var errorEmails = new Array(); //New array to contain the errors
+				var errorEmails = new Array(); // New array to contain the errors
 
 				var pattern = new RegExp(/^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?$/i);
 
 				for (var i = 0; i < arr.length; i++) {
-					//Check if the email is already added, only if dupEmailCheck is set to true
-					if ( dupEmailCheck === true && $orig.val().indexOf(arr[i]) != -1 ) {
+					// Check if the email is already added, only if checkForDuplicates is set to true
+					if ( checkForDuplicates === true && $orig.val().indexOf(arr[i]) != -1 ) {
 						if (arr[i] && arr[i].length > 0) {
 							new function () {
-								var existingElement = $list.find('.email_name[data-email=' + arr[i].toLowerCase().replace('.', '\\.').replace('@', '\\@') + ']');
+								var existingElement = $list.find('.email_name[data-email="' + arr[i].toLowerCase().replace('.', '\\.').replace('@', '\\@') + '"]');
 								existingElement.css('font-weight', 'bold');
 								setTimeout(function() { existingElement.css('font-weight', ''); }, 1500);
 							}(); // Use a IIFE function to create a new scope so existingElement won't be overriden
 						}
 					}
+					// Check if the email is a valid address, only if validate is set to true
 					else if (validate === true && pattern.test(arr[i]) == true) {
 						$list.append($('<li class="multiple_emails-email"><span class="email_name" data-email="' + arr[i].toLowerCase() + '">' + arr[i] + '</span></li>')
 							  .prepend($(deleteIconHTML)
